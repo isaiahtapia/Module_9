e7cf23560983335f1a7a4535b9815ba286fcd4a5
Isaiahtapia <isaiahjtapia@gmail.com>
2024-09-01 02:29:00 -0400

    finished codes functionality / need to record and edit readme

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
 // TODO: Include packages needed for this application
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+
 const questions = [
-   
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project:'
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your Github username:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a short description of your application:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please enter a step-by-step description for any installation procedures needed for your project:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any instructions on how to use the application:'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please enter any contributers who played a role in the creation of your application:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0','IBM', 'Mozilla Public', 'Other', 'None']
+    }
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(data) {
+    const markdown = generateMarkdown(data);
+    fs.writeFile('READMEFileSample.md', markdown, (error) => {
+        if (error) {
+            console.error('An error occurred', error);
+        } else {
+            console.log('README file created successfully.');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+    inquirer.prompt(questions)
+        .then(questionData => {
+            writeToFile(questionData);
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+    if (license === '') {
+        return '';
+    } else {
+        return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === '') {
+        return '';
+    } else {
+        return `[License](#license)`;
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === '') {
+        return '';
+    } else {
+        return `This project is licensed with a ${license} license.`;
+    }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(questionData) {
+    const licenseBadge = renderLicenseBadge(questionData.license);
+    const licenseSection = renderLicenseSection(questionData.license);
+   
+    return `
+    # README file
+  
+    # ${questionData.title} 
+  
+    # ${licenseBadge}
+  
+    # This README is created by ${questionData.name}.
+  
+    ## Table of Contents:
+  
+    - [Description](#description) 
+    - [Installation](#installation)  
+    - [Usage](#usage)
+    - [Contributions](#contributors) 
+    - [License](#license) 
+    
+    ## Description
+    
+    ${questionData.description}
+  
+    ## Installation
+    
+    ${questionData.install}
+  
+    ## Usage
+  
+    ${questionData.usage}
+  
+    ## Contribution:
+    
+    ${questionData.contributors}
+    
+    ## License
+    
+    ${licenseSection}
+    `;
 }
 
 module.exports = generateMarkdown;
